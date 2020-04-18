6720212e2ea48c023d0d0b5f85152150bbd8e0d5
S0021493556 <vijay.patne@wipro.com>
2020-04-18 10:49:37 +0000

    Import content for version 1.0.2

---
+++ test/integration/pages/FileUpload.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/test/Opa5"],function(e){"use strict";var i="FileUpload";e.createPageObjects({onTheAppPage:{actions:{},assertions:{iShouldSeeTheApp:function(){return this.waitFor({id:"app",viewName:i,success:function(){e.assert.ok(true,"The FileUpload view is displayed")},errorMessage:"Did not find the FileUpload view"})}}}})});
