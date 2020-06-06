8e284d4d963e7b14e6e74cbdf4e47b618b42a403
S0021493556 <vijay.patne@wipro.com>
2020-06-06 13:31:14 +0000

    Import content for version 1.0.12

---
+++ test/integration/opaTests.qunit.js
@@ -0,0 +1,1 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["Prod_Ord_Conf/Production_Ord_Conf/test/integration/AllJourneys"],function(){QUnit.start()})});
